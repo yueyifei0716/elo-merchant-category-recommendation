b4dab3d2d2d197e7a7e583d8d4b5140a43d8da9f
Yifei YUE <vincentyueyifei@gmail.com>
2022-07-27 01:18:12 +0800

    Add randomforest optimized by cross validation

---
+++ yifei/models/feature.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def feature_select_pearson(train, test):
+    """
+    利用pearson系数进行相关性特征选择
+    :param train:训练集
+    :param test:测试集
+    :return:经过特征选择后的训练集与测试集
+    """
+    print('feature_select...')
+    features = train.columns.tolist()
+    features.remove("card_id")
+    features.remove("target")
+    featureSelect = features[:]
+
+    # 去掉缺失值比例超过0.99的
+    for fea in features:
+        if train[fea].isnull().sum() / train.shape[0] >= 0.99:
+            featureSelect.remove(fea)
+
+    # 进行pearson相关性计算
+    corr = []
+    for fea in featureSelect:
+        corr.append(abs(train[[fea, 'target']].fillna(0).corr().values[0][1]))
+
+    # 取top300的特征进行建模，具体数量可选
+    se = pd.Series(corr, index=featureSelect).sort_values(ascending=False)
+    feature_select = ['card_id'] + se[:300].index.tolist()
+    print('done')
+    return train[feature_select + ['target']], test[feature_select]
--- yifei/models/random_forest.py
+++ yifei/models/random_forest.py
@@ -3,54 +3,59 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import numpy as np
+from feature import feature_select_pearson
 
 train = pd.read_csv("../preprocess/train.csv")
 test = pd.read_csv("../preprocess/test.csv")
 
-# 提取特征名称
-features = train.columns.tolist()
-features.remove("card_id")
-features.remove("target")
-featureSelect = features[:]
+train, test = feature_select_pearson(train, test)
 
-# 计算相关系数
-corr = []
-for fea in featureSelect:
-    corr.append(abs(train[[fea, 'target']].fillna(0).corr().values[0][1]))
-
-# 取top300的特征进行建模，具体数量可选
-se = pd.Series(corr, index=featureSelect).sort_values(ascending=False)
-feature_select = ['card_id'] + se[:100].index.tolist()
-
-# 输出结果
-train = train[feature_select + ['target']]
-test = test[feature_select]
-
+# Step 1.创建网格搜索空间
+print('param_grid_search')
 features = train.columns.tolist()
 features.remove("card_id")
 features.remove("target")
 
+# parameter_space = {
+#     "n_estimators": [79, 80, 81],
+#     "min_samples_leaf": [29, 30, 31],
+#     "min_samples_split": [2, 3],
+#     "max_depth": [9, 10],
+#     "max_features": ["auto", 80]
+# }
 
 parameter_space = {
-    "n_estimators": [79, 80, 81],
-    "min_samples_leaf": [29, 30, 31],
-    "min_samples_split": [2, 3],
-    "max_depth": [9, 10],
-    "max_features": ["auto", 80]
+    "n_estimators": [80],
+    "min_samples_leaf": [29],
+    "min_samples_split": [2],
+    "max_depth": [10],
+    "max_features": [80]
 }
 
+# Step 2.执行网格搜索过程
+print("Tuning hyper-parameters for mse")
+# 实例化随机森林模型
 clf = RandomForestRegressor(
-    criterion="mse",
+    criterion="squared_error",
     n_jobs=15,
     random_state=22)
-
+# 带入网格搜索
 grid = GridSearchCV(clf, parameter_space, cv=2, scoring="neg_mean_squared_error")
-grid.fit(train[features].values, train['target'].values)
+grid.fit(train[features], train['target'])
 
+# Step 3.输出网格搜索结果
+print("best_params_:")
 print(grid.best_params_)
+means = grid.cv_results_["mean_test_score"]
+stds = grid.cv_results_["std_test_score"]
+# 此处额外考虑观察交叉验证过程中不同超参数的
+for mean, std, params in zip(means, stds, grid.cv_results_["params"]):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+print('The best estimator is:')
 print(grid.best_estimator_)
 print('The score is:')
 print(np.sqrt(-grid.best_score_))
 
-test['target'] = grid.best_estimator_.predict(test[features])
-test[['card_id', 'target']].to_csv("../result/randomforest.csv", index=False)
+# test['target'] = grid.best_estimator_.predict(test[features])
+# test[['card_id', 'target']].to_csv("../result/randomforest.csv", index=False)
