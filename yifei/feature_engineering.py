a31af73c2b19bc06ee6bdcf5b35c7ce827a1ea06
Yifei YUE <vincentyueyifei@gmail.com>
2022-07-26 18:05:58 +0800

    Add feature engineering

---
+++ yifei/feature_engineering.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import gc
+import time
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+# In[33]:
+
+
+train = pd.read_csv('preprocess/train_pre.csv')
+test =  pd.read_csv('preprocess/test_pre.csv')
+transaction = pd.read_csv('preprocess/transaction_d_pre.csv')
+
+
+# In[34]:
+
+
+# 标注离散字段or连续型字段
+numeric_cols = ['purchase_amount', 'installments']
+
+category_cols = ['authorized_flag', 'city_id', 'category_1',
+       'category_3', 'merchant_category_id','month_lag','most_recent_sales_range',
+                 'most_recent_purchases_range', 'category_4',
+                 'purchase_month', 'purchase_hour_section', 'purchase_day']
+
+id_cols = ['card_id', 'merchant_id']
+
+
+# In[42]:
+
+
+transaction.loc[1].values
+
+
+# In[43]:
+
+
+transaction.shape[0]
+
+
+# In[44]:
+
+
+# 创建字典用于保存数据
+features = {}
+card_all = train['card_id'].append(test['card_id']).values.tolist()
+for card in card_all:
+    features[card] = {}
+
+# 标记不同类型字段的索引
+columns = transaction.columns.tolist()
+idx = columns.index('card_id')
+category_cols_index = [columns.index(col) for col in category_cols]
+numeric_cols_index = [columns.index(col) for col in numeric_cols]
+
+# 记录运行时间
+s = time.time()
+num = 0
+
+# 执行循环，并在此过程中记录时间
+for i in range(transaction.shape[0]):
+    va = transaction.loc[i].values
+    card = va[idx]
+    for cate_ind in category_cols_index:
+        for num_ind in numeric_cols_index:
+            # print(type(columns[cate_ind]), type(va[cate_ind]), type(columns[num_ind]))
+            col_name = '&'.join([columns[cate_ind], str(va[cate_ind]), columns[num_ind]])
+            features[card][col_name] = features[card].get(col_name, 0) + va[num_ind]
+    num += 1
+    if num%1000000==0:
+        print(time.time()-s, "s")
+del transaction
+gc.collect()
+
+
+# In[45]:
+
+
+# 字典转dataframe
+df = pd.DataFrame(features).T.reset_index()
+del features
+cols = df.columns.tolist()
+df.columns = ['card_id'] + cols[1:]
+
+# 生成训练集与测试集
+train = pd.merge(train, df, how='left', on='card_id')
+test =  pd.merge(test, df, how='left', on='card_id')
+del df
+train.to_csv("preprocess/train_dict.csv", index=False)
+test.to_csv("preprocess/test_dict.csv", index=False)
+
+gc.collect()
+
+
+# In[46]:
+
+
+transaction = pd.read_csv('preprocess/transaction_g_pre.csv')
+
+
+# In[47]:
+
+
+# 标注离散字段or连续型字段
+numeric_cols = ['authorized_flag',  'category_1', 'installments',
+       'category_3',  'month_lag','purchase_month','purchase_day','purchase_day_diff', 'purchase_month_diff',
+       'purchase_amount', 'category_2',
+       'purchase_month', 'purchase_hour_section', 'purchase_day',
+       'most_recent_sales_range', 'most_recent_purchases_range', 'category_4']
+categorical_cols = ['city_id', 'merchant_category_id', 'merchant_id', 'state_id', 'subsector_id']
+
+
+# In[48]:
+
+
+# 创建空字典
+aggs = {}
+
+# 连续/离散字段统计量提取范围
+for col in numeric_cols:
+    aggs[col] = ['nunique', 'mean', 'min', 'max','var','skew', 'sum']
+for col in categorical_cols:
+    aggs[col] = ['nunique']
+aggs['card_id'] = ['size', 'count']
+cols = ['card_id']
+
+# 借助groupby实现统计量计算
+for key in aggs.keys():
+    cols.extend([key+'_'+stat for stat in aggs[key]])
+
+df = transaction[transaction['month_lag']<0].groupby('card_id').agg(aggs).reset_index()
+df.columns = cols[:1] + [co+'_hist' for co in cols[1:]]
+
+df2 = transaction[transaction['month_lag']>=0].groupby('card_id').agg(aggs).reset_index()
+df2.columns = cols[:1] + [co+'_new' for co in cols[1:]]
+df = pd.merge(df, df2, how='left',on='card_id')
+
+df2 = transaction.groupby('card_id').agg(aggs).reset_index()
+df2.columns = cols
+df = pd.merge(df, df2, how='left',on='card_id')
+del transaction
+gc.collect()
+
+# 生成训练集与测试集
+train = pd.merge(train, df, how='left', on='card_id')
+test =  pd.merge(test, df, how='left', on='card_id')
+del df
+train.to_csv("preprocess/train_groupby.csv", index=False)
+test.to_csv("preprocess/test_groupby.csv", index=False)
+
+gc.collect()
+
+
+# In[49]:
+
+
+train_dict = pd.read_csv("preprocess/train_dict.csv")
+test_dict = pd.read_csv("preprocess/test_dict.csv")
+train_groupby = pd.read_csv("preprocess/train_groupby.csv")
+test_groupby = pd.read_csv("preprocess/test_groupby.csv")
+
+
+# In[50]:
+
+
+for co in train_dict.columns:
+    if co in train_groupby.columns and co!='card_id':
+        del train_groupby[co]
+for co in test_dict.columns:
+    if co in test_groupby.columns and co!='card_id':
+        del test_groupby[co]
+
+
+# In[51]:
+
+
+train = pd.merge(train_dict, train_groupby, how='left', on='card_id').fillna(0)
+test = pd.merge(test_dict, test_groupby, how='left', on='card_id').fillna(0)
+
+
+# In[52]:
+
+
+train.to_csv("preprocess/train.csv", index=False)
+test.to_csv("preprocess/test.csv", index=False)
+
+del train_dict, test_dict, train_groupby, test_groupby
+gc.collect()
+
+
+# In[ ]:
+
+
+
+
--- yifei/preprocessing.py
+++ yifei/preprocessing.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
 import gc
+import time
 import numpy as np
 import pandas as pd
 from datetime import datetime
+from helper import change_object_cols
+
+
+# In[13]:
+
 
 train = pd.read_csv('../elo-merchant-category-recommendation/train.csv')
 test = pd.read_csv('../elo-merchant-category-recommendation/test.csv')
@@ -9,11 +21,7 @@ merchant = pd.read_csv('../elo-merchant-category-recommendation/merchants.csv')
 transaction = pd.read_csv('../elo-merchant-category-recommendation/new_merchant_transactions.csv')
 
 
-# 字典编码函数
-def change_object_cols(se):
-    value = se.unique().tolist()
-    value.sort()
-    return se.map(pd.Series(range(len(value)), index=value)).values
+# In[14]:
 
 
 # 对首次活跃月份进行编码
@@ -21,22 +29,34 @@ se_map = change_object_cols(train['first_active_month'].append(test['first_activ
 train['first_active_month'] = se_map[:train.shape[0]]
 test['first_active_month'] = se_map[train.shape[0]:]
 
+
+# In[15]:
+
+
 train.to_csv("preprocess/train_pre.csv", index=False)
 test.to_csv("preprocess/test_pre.csv", index=False)
+
+
+# In[16]:
+
 
 del train
 del test
 gc.collect()
 
+
+# In[17]:
+
+
 # 1、根据业务含义划分离散字段category_cols与连续字段numeric_cols。
 category_cols = ['merchant_id', 'merchant_group_id', 'merchant_category_id',
-                 'subsector_id', 'category_1',
-                 'most_recent_sales_range', 'most_recent_purchases_range',
-                 'category_4', 'city_id', 'state_id', 'category_2']
+       'subsector_id', 'category_1',
+       'most_recent_sales_range', 'most_recent_purchases_range',
+       'category_4', 'city_id', 'state_id', 'category_2']
 numeric_cols = ['numerical_1', 'numerical_2',
-                'avg_sales_lag3', 'avg_purchases_lag3', 'active_months_lag3',
-                'avg_sales_lag6', 'avg_purchases_lag6', 'active_months_lag6',
-                'avg_sales_lag12', 'avg_purchases_lag12', 'active_months_lag12']
+     'avg_sales_lag3', 'avg_purchases_lag3', 'active_months_lag3',
+       'avg_sales_lag6', 'avg_purchases_lag6', 'active_months_lag6',
+       'avg_sales_lag12', 'avg_purchases_lag12', 'active_months_lag12']
 
 # 2、对非数值型的离散字段进行字典排序编码。
 for col in ['category_1', 'most_recent_sales_range', 'most_recent_purchases_range', 'category_4']:
@@ -44,6 +64,7 @@ for col in ['category_1', 'most_recent_sales_range', 'most_recent_purchases_rang
 
 # 3、为了能够更方便统计，进行缺失值的处理，对离散字段统一用-1进行填充。
 merchant[category_cols] = merchant[category_cols].fillna(-1)
+
 
 # 4、对离散型字段探查发现有正无穷值，这是特征提取以及模型所不能接受的，因此需要对无限值进行处理，此处采用最大值进行替换。
 inf_cols = ['avg_purchases_lag3', 'avg_purchases_lag6', 'avg_purchases_lag12']
@@ -54,22 +75,19 @@ for col in numeric_cols:
     merchant[col] = merchant[col].fillna(merchant[col].mean())
 
 # 6、去除与transaction交易记录表格重复的列，以及merchant_id的重复记录。
-duplicate_cols = ['merchant_id', 'merchant_category_id', 'subsector_id', 'category_1', 'city_id', 'state_id',
-                  'category_2']
+duplicate_cols = ['merchant_id', 'merchant_category_id', 'subsector_id', 'category_1', 'city_id', 'state_id', 'category_2']
 merchant = merchant.drop(duplicate_cols[1:], axis=1)
 merchant = merchant.loc[merchant['merchant_id'].drop_duplicates().index.tolist()].reset_index(drop=True)
 
-# 1、为了统一处理，首先拼接new和history两张表格，后续可以month_lag>=0进行区分。
-# transaction = pd.concat([new_transaction, history_transaction], axis=0, ignore_index=True)
-# del new_transaction
-# del history_transaction
-# gc.collect()
+
+# In[18]:
+
 
 # 2、同样划分离散字段、连续字段以及时间字段。
-numeric_cols = ['installments', 'month_lag', 'purchase_amount']
+numeric_cols = [ 'installments', 'month_lag', 'purchase_amount']
 category_cols = ['authorized_flag', 'card_id', 'city_id', 'category_1',
-                 'category_3', 'merchant_category_id', 'merchant_id', 'category_2', 'state_id',
-                 'subsector_id']
+       'category_3', 'merchant_category_id', 'merchant_id', 'category_2', 'state_id',
+       'subsector_id']
 time_cols = ['purchase_date']
 
 # 3、可仿照merchant的处理方式对字符型的离散特征进行字典序编码以及缺失值填充。
@@ -80,15 +98,24 @@ transaction['category_2'] = transaction['category_2'].astype(int)
 
 # 4、进行时间段的处理，简单起见进行月份、日期的星期数（工作日与周末）、以及
 # 时间段（上午、下午、晚上、凌晨）的信息提取。
-transaction['purchase_month'] = transaction['purchase_date'].apply(lambda x: '-'.join(x.split(' ')[0].split('-')[:2]))
-transaction['purchase_hour_section'] = transaction['purchase_date'].apply(
-    lambda x: x.split(' ')[1].split(':')[0]).astype(int) // 6
-transaction['purchase_day'] = transaction['purchase_date'].apply(
-    lambda x: datetime.strptime(x.split(" ")[0], "%Y-%m-%d").weekday()) // 5
+transaction['purchase_month'] = transaction['purchase_date'].apply(lambda x:'-'.join(x.split(' ')[0].split('-')[:2]))
+transaction['purchase_hour_section'] = transaction['purchase_date'].apply(lambda x: x.split(' ')[1].split(':')[0]).astype(int)//6
+transaction['purchase_day'] = transaction['purchase_date'].apply(lambda x: datetime.strptime(x.split(" ")[0], "%Y-%m-%d").weekday())//5
 del transaction['purchase_date']
+
 
 # 5、对新生成的购买月份离散字段进行字典序编码。
 transaction['purchase_month'] = change_object_cols(transaction['purchase_month'].fillna(-1).astype(str))
+
+
+# In[18]:
+
+
+
+
+
+# In[19]:
+
 
 # 为了方便特征的统一计算将其merge合并，重新划分相应字段种类。
 cols = ['merchant_id', 'most_recent_sales_range', 'most_recent_purchases_range', 'category_4']
@@ -97,16 +124,119 @@ transaction = pd.merge(transaction, merchant[cols], how='left', on='merchant_id'
 numeric_cols = ['purchase_amount', 'installments']
 
 category_cols = ['authorized_flag', 'city_id', 'category_1',
-                 'category_3', 'merchant_category_id', 'month_lag', 'most_recent_sales_range',
+       'category_3', 'merchant_category_id','month_lag','most_recent_sales_range',
                  'most_recent_purchases_range', 'category_4',
                  'purchase_month', 'purchase_hour_section', 'purchase_day']
 
 id_cols = ['card_id', 'merchant_id']
 
 transaction[cols[1:]] = transaction[cols[1:]].fillna(-1).astype(int)
-transaction[category_cols] = transaction[category_cols].fillna(-1).astype(str)
-
-transaction.to_csv("preprocess/transaction_pre.csv", index=False)
-
+transaction[category_cols] =transaction[category_cols].fillna(-1).astype(str)
+transaction.to_csv("preprocess/transaction_d_pre.csv", index=False)
 del transaction
 gc.collect()
+
+
+# In[ ]:
+
+
+
+
+
+# In[24]:
+
+
+merchant = pd.read_csv('../elo-merchant-category-recommendation/merchants.csv')
+transaction = pd.read_csv('../elo-merchant-category-recommendation/new_merchant_transactions.csv')
+
+
+# In[25]:
+
+
+# 1、根据业务含义划分离散字段category_cols与连续字段numeric_cols。
+category_cols = ['merchant_id', 'merchant_group_id', 'merchant_category_id',
+       'subsector_id', 'category_1',
+       'most_recent_sales_range', 'most_recent_purchases_range',
+       'category_4', 'city_id', 'state_id', 'category_2']
+numeric_cols = ['numerical_1', 'numerical_2',
+     'avg_sales_lag3', 'avg_purchases_lag3', 'active_months_lag3',
+       'avg_sales_lag6', 'avg_purchases_lag6', 'active_months_lag6',
+       'avg_sales_lag12', 'avg_purchases_lag12', 'active_months_lag12']
+
+# 2、对非数值型的离散字段进行字典排序编码。
+for col in ['category_1', 'most_recent_sales_range', 'most_recent_purchases_range', 'category_4']:
+    merchant[col] = change_object_cols(merchant[col])
+
+# 3、为了能够更方便统计，进行缺失值的处理，对离散字段统一用-1进行填充。
+merchant[category_cols] = merchant[category_cols].fillna(-1)
+
+
+# 4、对离散型字段探查发现有正无穷值，这是特征提取以及模型所不能接受的，因此需要对无限值进行处理，此处采用最大值进行替换。
+inf_cols = ['avg_purchases_lag3', 'avg_purchases_lag6', 'avg_purchases_lag12']
+merchant[inf_cols] = merchant[inf_cols].replace(np.inf, merchant[inf_cols].replace(np.inf, -99).max().max())
+
+# 5、平均值进行填充，后续有需要再进行优化处理。
+for col in numeric_cols:
+    merchant[col] = merchant[col].fillna(merchant[col].mean())
+
+# 6、去除与transaction交易记录表格重复的列，以及merchant_id的重复记录。
+duplicate_cols = ['merchant_id', 'merchant_category_id', 'subsector_id', 'category_1', 'city_id', 'state_id', 'category_2']
+merchant = merchant.drop(duplicate_cols[1:], axis=1)
+merchant = merchant.loc[merchant['merchant_id'].drop_duplicates().index.tolist()].reset_index(drop=True)
+
+
+# In[26]:
+
+
+# 2、同样划分离散字段、连续字段以及时间字段。
+numeric_cols = [ 'installments', 'month_lag', 'purchase_amount']
+category_cols = ['authorized_flag', 'card_id', 'city_id', 'category_1',
+       'category_3', 'merchant_category_id', 'merchant_id', 'category_2', 'state_id',
+       'subsector_id']
+time_cols = ['purchase_date']
+
+# 3、可仿照merchant的处理方式对字符型的离散特征进行字典序编码以及缺失值填充。
+for col in ['authorized_flag', 'category_1', 'category_3']:
+    transaction[col] = change_object_cols(transaction[col].fillna(-1).astype(str))
+transaction[category_cols] = transaction[category_cols].fillna(-1)
+transaction['category_2'] = transaction['category_2'].astype(int)
+
+# 4、进行时间段的处理，简单起见进行月份、日期的星期数（工作日与周末）、以及
+# 时间段（上午、下午、晚上、凌晨）的信息提取。
+transaction['purchase_month'] = transaction['purchase_date'].apply(lambda x:'-'.join(x.split(' ')[0].split('-')[:2]))
+transaction['purchase_hour_section'] = transaction['purchase_date'].apply(lambda x: x.split(' ')[1].split(':')[0]).astype(int)//6
+transaction['purchase_day'] = transaction['purchase_date'].apply(lambda x: datetime.strptime(x.split(" ")[0], "%Y-%m-%d").weekday())//5
+del transaction['purchase_date']
+
+
+# 5、对新生成的购买月份离散字段进行字典序编码。
+transaction['purchase_month'] = change_object_cols(transaction['purchase_month'].fillna(-1).astype(str))
+
+
+# In[27]:
+
+
+cols = ['merchant_id', 'most_recent_sales_range', 'most_recent_purchases_range', 'category_4']
+transaction = pd.merge(transaction, merchant[cols], how='left', on='merchant_id')
+
+numeric_cols = ['purchase_amount', 'installments']
+
+category_cols = ['authorized_flag', 'city_id', 'category_1',
+       'category_3', 'merchant_category_id','month_lag','most_recent_sales_range',
+                 'most_recent_purchases_range', 'category_4',
+                 'purchase_month', 'purchase_hour_section', 'purchase_day']
+
+id_cols = ['card_id', 'merchant_id']
+
+transaction['purchase_day_diff'] = transaction.groupby("card_id")['purchase_day'].diff()
+transaction['purchase_month_diff'] = transaction.groupby("card_id")['purchase_month'].diff()
+transaction.to_csv("preprocess/transaction_g_pre.csv", index=False)
+del transaction
+gc.collect()
+
+
+# In[ ]:
+
+
+
+
