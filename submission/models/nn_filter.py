5d6496c7859ff21b09b54c563f641adbd8c6a50e
Yifei YUE <vincentyueyifei@gmail.com>
2022-08-01 15:47:55 +0800

    Add

--- submission/models/nn_filter.py
+++ submission/models/nn_filter.py
@@ -1,19 +1,41 @@
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 import pylab as pl
 from pathlib import Path
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 pd.set_option('display.max_columns', None)
 
+
+def feature_select_pearson(train):
+    print('feature_select...')
+    features = train.columns.tolist()
+    features.remove("card_id")
+    features.remove("target")
+    featureSelect = features[:]
+
+    for fea in features:
+        if train[fea].isnull().sum() / train.shape[0] >= 0.99:
+            featureSelect.remove(fea)
+
+    corr = []
+    for fea in featureSelect:
+        corr.append(abs(train[[fea, 'target']].fillna(0).corr().values[0][1]))
+
+    se = pd.Series(corr, index=featureSelect).sort_values(ascending=False)
+    feature_select = ['card_id'] + se[:300].index.tolist()
+    print('done')
+    return train[feature_select + ['target']]
+
+
 data_folder = Path("../preprocess/")
-origian_data = pd.read_csv(data_folder / 'train_pre.csv')
+origian_data = pd.read_csv(data_folder / 'new_train.csv')
 col_num = origian_data.shape[0]
-origian_data = origian_data.drop('card_id', axis=1)
+origian_data = feature_select_pearson(origian_data).drop('card_id', axis=1)
 
 train = np.array(origian_data.iloc[:, :]).astype(float)
 train_all = np.array(origian_data.iloc[:, :]).astype(float)
@@ -36,7 +58,7 @@ class Net(torch.nn.Module):
         return x
 
 
-net = Net(4, 100, 1)
+net = Net(300, 100, 1)
 optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
 loss_function = torch.nn.MSELoss()
 loss_value = []
